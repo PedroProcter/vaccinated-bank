76ff69424754289ce5bd26eff0044b7ca44ac7ea
Albert2707 <202010652@itla.edu.do>
2022-03-16 20:18:56 -0400

    Datos

---
+++ Data/Province.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vaccinated_bank.Data
+{
+    public partial class Province
+    {
+        public int IdProvincia { get; set; }
+        public string? Province1 { get; set; }
+    }
+}
---
+++ Data/Vaccinated.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vaccinated_bank.Data
+{
+    public partial class Vaccinated
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Cedula { get; set; }
+        public string? Phone { get; set; }
+        public DateOnly? BirthDate { get; set; }
+        public string? Vaccine { get; set; }
+        public string? Province { get; set; }
+        public string? ZodiacSign { get; set; }
+        public DateOnly? DateVaccine { get; set; }
+    }
+}
